422f510d452af776c1aacc5485733d5351430e64
madayuki1 <madayukii12@gmail.com>
2026-05-08 17:30:27 +0800

    1

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import Task
 
 # Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    fields = ['title', 'completed']
+    pass
 
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
