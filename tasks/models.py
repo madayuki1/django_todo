1be188cb16aa84e9c396885ae19e715479418548
madayuki1 <madayukii12@gmail.com>
2026-05-25 17:53:35 +0800

    delete button

--- tasks/models.py
+++ tasks/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
+    due_date = models.DateField(default=timezone.now())
     completed = models.BooleanField(default=False)
     
     def __str__(self):
