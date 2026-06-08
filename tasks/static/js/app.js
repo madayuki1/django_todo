71700ee7e2e1a83dec35a558f0992f168230faf5
madayuki1 <madayukii12@gmail.com>
2026-06-08 17:29:01 +0800

    update

--- tasks/static/js/app.js
+++ tasks/static/js/app.js
@@ -1,6 +1,7 @@
 const open_modal = document.getElementById('open_modal_create');
 const close_modal = document.getElementById('close_modal_create')
 const modal_overlay = document.getElementById('modal-overlay');
+const edit_button = document.querySelector('.btn-edit');
 
 
 open_modal.addEventListener("click", () => {
@@ -9,4 +10,8 @@ open_modal.addEventListener("click", () => {
 
 close_modal.addEventListener("click", () => {
     modal_overlay.classList.remove("active")
+});
+
+edit_button.addEventListener("click", () => {
+    modal_overlay.classList.add("active");
 });
