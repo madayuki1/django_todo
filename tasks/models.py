0282c3378e174d1c4ff69dbef0b01a9cfa5157a4
madayuki <abed.kurniawan76@gmail.com>
2026-06-12 14:16:32 +0800

    test

--- tasks/models.py
+++ tasks/models.py
@@ -2,14 +2,36 @@ from django.db import models
 from django.utils import timezone
 from datetime import timedelta
 
+
 # Create your models here.
+class Category(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+    def get_id(self):
+        return self.id
+
 
 class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=200)
     due_date = models.DateField(default=timezone.now())
     completed = models.BooleanField(default=False)
-    
+
+    category = models.ForeignKey(
+        to=Category,
+        verbose_name=("Categories"),
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+
     def __str__(self):
         return self.title
 
@@ -22,7 +44,7 @@ class Task(models.Model):
         delta = self.due_date - now
 
         return delta.days
-    
+
     @property
     def remaining_days(self):
         if self.completed:
@@ -35,16 +57,3 @@ class Task(models.Model):
             return "Today"
         else:
             return f"{days} Days"
-
-class Category(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=200)
-
-    class Meta:
-        verbose_name_plural = "Categories"
-
-    def __str__(self):
-        return self.name
-    
-    def get_id(self):
-        return self.id
